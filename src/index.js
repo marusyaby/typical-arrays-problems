3c8d6bfa3e890ed4093ae3b5d2c906e69e5fe52d
marusyaby <mariakhrol@gmail.com>
2020-10-04 17:11:34 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    return array.sort((a, b) => a - b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    return array.sort((a, b) => a - b)[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    return array.reduce((a, b) => a + b, 0) / array.length;
 }
